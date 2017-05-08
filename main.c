bb2b383088b47df0f17a462f1392d4f8c8b80cb7
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-05-08 22:28:41 +0200

    test with variable and the command emcc main.c -o salut.html which will create an html with the printf printed onto the screen

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+void loop (int);
+int testInt = 666;
 
 int main() {
-  printf("hello, world!\n");
+  printf("the testInt value is %d\n", testInt);
+  loop(6);
   return 0;
+}
+
+void loop (int x){
+
+for (int i = 0; i < x; i++) {
+printf("hello for the %d time\n", i);
+}
+
 }
